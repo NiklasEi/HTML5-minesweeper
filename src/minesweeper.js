a93ae92710e6c32624fc8d46d804643dd279b5bd
Niklas Eicker <nikle1@gmx.de>
2018-11-06 00:11:46 +0100

    fix offset in touch and clicks

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -285,6 +285,9 @@ function onClick(event) {
     x -= canvas.offsetLeft;
     y -= canvas.offsetTop;
 
+    x -= offsetX;
+    y -= offsetY;
+
     if (event.button === 0) uncover(Math.floor(x / sizePerBox), Math.floor(y / sizePerBox));
     if (event.button === 2) flag(Math.floor(x / sizePerBox), Math.floor(y / sizePerBox));
 }
@@ -307,6 +310,9 @@ function onTouchStart(event) {
         x -= canvas.offsetLeft;
         y -= canvas.offsetTop;
 
+        x -= offsetX;
+        y -= offsetY;
+
         currentTouchTimer = setTimeout(function() { touchFlag(Math.floor(x / sizePerBox), Math.floor(y / sizePerBox)); }, longTouchDuration);
         event.preventDefault();
     }
@@ -326,6 +332,9 @@ function onTouchEnd(event) {
 
         x -= canvas.offsetLeft;
         y -= canvas.offsetTop;
+
+        x -= offsetX;
+        y -= offsetY;
 
         clearTimeout(currentTouchTimer);
         if (!ignoreTouchEnd) uncover(Math.floor(x / sizePerBox), Math.floor(y / sizePerBox));
